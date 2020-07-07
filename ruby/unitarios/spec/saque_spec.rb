82412d63a80f67e50d10a2e5583478b385dd41b2
Sandson Costa <sandson.costa@outlook.com>
2020-07-06 22:13:11 -0300

    Refatorando o código

--- ruby/unitarios/spec/saque_spec.rb
+++ ruby/unitarios/spec/saque_spec.rb
@@ -51,23 +51,23 @@ describe ContaCorrente do
                 @conta = ContaCorrente.new(100.00)
                 @conta.saca(101.00)
             end
-            it 'vejo mensagem' do
+            it 'então exibe mensagem' do
                 expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
             end
-            it 'meu saldo permanece conforme o valor inicial' do
+            it 'e o saldo permanece' do
                 expect(@conta.saldo).to eql 100.00
             end
         end
 
-        context 'quando o valor do saque é maior que o limite por transação' do
+        context 'quando supera o limite de saque' do
             before(:all) do
                 @conta = ContaCorrente.new(1000.00)
                 @conta.saca(701.00)
             end
-            it 'vejo mensagem' do
+            it 'então exibe mensagem' do
                 expect(@conta.mensagem).to eql 'Limite máximo por saque é de 700 reais.'
             end
-            it 'meu saldo permanece conforme o valor inicial' do
+            it 'e o saldo permanece' do
                 expect(@conta.saldo).to eql 1000.00
             end
         end
